b95c673572a5b7936c2874e39dad16bc061c7fc6
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2025-09-08 16:32:48 -0700

    Docstrings for bag.rs

--- src/bag.rs
+++ src/bag.rs
@@ -2,17 +2,22 @@ use rand::{rng, seq::SliceRandom};
 
 use crate::protocol::ProtocolFormat;
 
+/// This struct is for handling a shuffled `Vec<T>` of items.
+/// Items are removed from the bag when accessed and bags may be restocked at any time.
 #[derive(Debug)]
 pub struct Bag<T> {
     items: Vec<T>,
 }
 
 impl<T> Bag<T> {
+    /// Creates a new bag from `items` after shuffling them.
     pub fn new(mut items: Vec<T>) -> Self {
         items.shuffle(&mut rng());
         Bag { items }
     }
 
+    /// Restocks the bag with the given `items` after shuffling them.  
+    /// Items previously in this bag are not retained.
     pub fn restock(&mut self, mut items: Vec<T>) {
         items.shuffle(&mut rng());
         self.items = items;
