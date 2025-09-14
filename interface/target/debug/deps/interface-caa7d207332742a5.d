d7970adca2f421ac53f4fa3d8523d9a157c3d98b
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2025-09-14 14:30:09 -0700

    No more tracking target

--- interface/target/debug/deps/interface-caa7d207332742a5.d
+++
@@ -1,9 +0,0 @@
-C:\Users\tyler\rust projects\azul\interface\target\debug\deps\interface-caa7d207332742a5.d: src\main.rs Cargo.toml
-
-C:\Users\tyler\rust projects\azul\interface\target\debug\deps\libinterface-caa7d207332742a5.rmeta: src\main.rs Cargo.toml
-
-src\main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
