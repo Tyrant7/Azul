fb7e57a115c39313e53da4affb868312ae1578ad
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2025-09-14 14:20:35 -0700

    Began to setup interface project

---
+++ interface/target/debug/deps/interface-f817e1533fd1e990.d
@@ -0,0 +1,9 @@
+C:\Users\tyler\rust projects\azul\interface\target\debug\deps\interface-f817e1533fd1e990.d: src\main.rs Cargo.toml
+
+C:\Users\tyler\rust projects\azul\interface\target\debug\deps\libinterface-f817e1533fd1e990.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
