6c1d30d7eda8ae668d331ded321e162d30678297
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-08-29 15:37:09 -0700

    Boardstate encoding

--- rl_env/src/lib.rs
+++ rl_env/src/lib.rs
@@ -1,40 +1,144 @@
-use std::ops::{Add, Div};
+use std::{
+    char::MAX,
+    ops::{Add, Div},
+};
 
 use rand::seq::IndexedRandom;
 
-use azul_movegen::{Bowl, GameState, Move};
+use azul_movegen::{Bag, Board, Bowl, GameState, Move};
 use tch::Tensor;
 
+const MAX_PLAYERS: usize = 4;
+const TILE_TYPES: usize = 5;
+
 fn encode_gamestate(gamestate: &GameState) -> Tensor {
-    let bowls = gamestate.get_bowls();
-    let boards = gamestate.get_boards();
-    let encoded_bowls = encode_bowls(bowls);
-    let encoded_boards = encode_boards(&boards);
-    Tensor::cat(&[encoded_bowls, encoded_boards], 0)
+    let encoded_bowls = encode_bowls(gamestate.get_bowls());
+    let encoded_boards = encode_boards(&gamestate.get_boards());
+    let encoded_bag = encode_bag(gamestate.get_bag());
+    let player_count_encoding = Tensor::zeros(MAX_PLAYERS, (tch::Kind::Float, get_device()));
+    player_count_encoding
+        .get(gamestate.get_player_count() as i64)
+        .add(1.);
+    Tensor::cat(
+        &[
+            encoded_bowls,
+            encoded_boards,
+            encoded_bag,
+            player_count_encoding,
+        ],
+        0,
+    )
 }
 
 fn encode_bowls(bowls: &Vec<Bowl>) -> Tensor {
     // Max bowls including floor, with 1 space normalized per tile-type per bowl
-    let size = (2 * 4 + 2) * 5;
+    let size = (2 * MAX_PLAYERS + 2) * TILE_TYPES;
     let encoded_bowls = Tensor::zeros(size, (tch::Kind::Float, get_device()));
     for (bowl_index, bowl) in bowls.iter().enumerate() {
         for tile_type in bowl.get_tiles().iter() {
-            let index = bowl_index * 5 + tile_type;
+            let index = bowl_index * TILE_TYPES + tile_type;
             encoded_bowls.get(index as i64).add(1.);
         }
     }
 
     // Normalize by tiles by bowl
-    encoded_bowls.i(5..).div(4.);
+    encoded_bowls.i(TILE_TYPES..).div(4.);
 
     // Centre can hold up to 20 tiles of one type
-    encoded_bowls.i(0..5).div(20.);
+    encoded_bowls.i(0..TILE_TYPES).div(20.);
 
     encoded_bowls
 }
 
 fn encode_boards(boards: &[Board]) -> Tensor {
-    unimplemented!()
+    // Board occupancy map -> 5 rows and 5 cols, with 5 tiletypes
+    let occupancy_size = 5 * 5 * TILE_TYPES;
+    // Holds -> one-hot on tile type plus a normalized fullness
+    let holds_size = TILE_TYPES + 1;
+    // Penalties -> single normalized value
+    let penalties_size = 1;
+    // Score -> single scaled relative-scoring value between this board and each other board
+    let score_size = MAX_PLAYERS;
+    // Bonus types -> 5 rows, 5 cols, and 5 tiletypes
+    let bonus_types_size = 5 * 5 * TILE_TYPES;
+
+    let encoded_boards = Vec<Tensor>::with_capacity(boards.len());
+    for board in boards {
+        // Occupancy
+        let encoded_occupancy = Tensor::zeros(occupancy_size, (tch::Kind::Float, get_device()));
+        for (row_index, row) in board.get_placed().iter().enumerate() {
+            for (col_index, tile) in row.iter().enumerate() {
+                if let Some(tile) = tile {
+                    let index = row_index * 5 * TILE_TYPES + col_index * TILE_TYPES + tile;
+                    encoded_boards.get(index as i64).add(1.);
+                }
+            }
+        }
+
+        // Holds
+        let encoded_holds = Tensor::zeros(holds_size, (tch::Kind::Float, get_device()));
+        for (row_index, row) in board.get_holds().iter().enumerate() {
+            for tile in row.iter().flatten() {
+                let index = row_index * TILE_TYPES + tile;
+                encoded_holds.get(index as i64).add(1.);
+            }
+        }
+
+        // Penalties
+        let encoded_penalties = Tensor::zeros(penalties_size, (tch::Kind::Float, get_device()));
+        let penalty = board.get_penalty();
+        encoded_penalties.get(0).add(penalty);
+
+        // Scores
+        // This might have a bit of an issue since the score is relative but unordered so different boards will have the zero relative score encoded in a different position based on their index in the list of boards
+        let encoded_scores = Tensor::zeros(score_size, (tch::Kind::Float, get_device()));
+        let score = board.get_score();
+        for (i, other_board) in boards.iter().enumerate() {
+            let other_score = other_board.get_score();
+            let relative_score = score as f32 - other_score as f32;
+            encoded_scores.get(i as i64).add(relative_score);
+        }
+
+        // Bonuses
+        let bonuses = board.get_bonuses();
+        let encoded_bonuses = Tensor::from_slice(
+            &bonuses
+                .rows
+                .iter()
+                .chain(bonuses.columns.iter())
+                .chain(bonuses.tile_types.iter())
+                .map(|&b| if b { 1.0 } else { 0.0 })
+                .collect::<Vec<f32>>(),
+        );
+
+        encoded_boards.push(Tensor::cat(
+            &[
+                encoded_occupancy,
+                encoded_holds,
+                encoded_penalties,
+                encoded_scores,
+                encoded_bonuses,
+            ],
+            0,
+        ));
+    }
+
+    Tensor::from_slice(encoded_boards.collect::<Vec<Tensor>>())
+        .to_device(get_device())
+}
+
+fn encode_bag(bag: &Bag) -> Tensor {
+    let size = TILE_TYPES;
+    let encoded_bag = Tensor::zeros(size, (tch::Kind::Float, get_device()));
+    for tile_type in bag.items().iter() {
+        let index = tile_type * TILE_TYPES;
+        encoded_bag.get(index as i64).add(1.);
+    }
+
+    // Normalize by max tiles in bag
+    encoded_bag.div(20.);
+
+    encoded_bag
 }
 
 fn calculate_reward(gamestate: &GameState) -> f32 {
@@ -103,8 +207,8 @@ impl AzulEnv {
         // tile_type in [0, 4]
         // row in [0, 5] (0 = floor, 1-5 = wall)
         // so action in [0, 9*5*6) = [0, 270)
-        let bowl = action / (5 * 6);
-        let tile_type = (action / 6) % 5;
+        let bowl = action / (TILE_TYPES * 6);
+        let tile_type = (action / 6) % TILE_TYPES;
         let row = match action % 6 {
             0 => azul_movegen::row::Row::Floor,
             n => azul_movegen::row::Row::Wall(n - 1),
