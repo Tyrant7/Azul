a35947eb8dee3de32aba10ca411f9d2e27094247
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-08-30 17:02:48 -0700

    Step 2

--- rl_env/src/ppo.rs
+++ rl_env/src/ppo.rs
@@ -10,4 +10,10 @@ pub fn ppo() {
     let critic_vs = VarStore::new(get_device());
     let actor = initialize_actor(&actor_vs.root());
     let critic = initialize_critic(&critic_vs.root());
+
+    let max_steps = 10000;
+    let mut steps = 0;
+    while steps < max_steps {
+        steps += 1;
+    }
 }
