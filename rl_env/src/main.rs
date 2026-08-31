3006eccde9d48a4c0bbd63a5cacb5c080e69a1a5
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-08-31 12:07:48 -0700

    PPO program entrypoint

---
+++ rl_env/src/main.rs
@@ -0,0 +1,14 @@
+fn main() -> Result<(), tch::TchError> {
+    let config = rl_env::PpoConfig {
+        timesteps_per_batch: 1_000,
+        max_timesteps_per_episode: 1_000,
+        updates_per_iteration: 5,
+        ..Default::default()
+    };
+
+    let mut trainer = rl_env::PpoTrainer::new(config)?;
+    let mut environment = rl_env::AzulEnv::new(0, config.max_timesteps_per_episode);
+
+    trainer.train(&mut environment, 100_000);
+    Ok(())
+}
