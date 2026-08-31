f336a25a221b2dc1b0033f2be21401960eb0dcde
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-08-31 14:24:39 -0700

    Thinking

--- rl_env/src/main.rs
+++ rl_env/src/main.rs
@@ -15,7 +15,7 @@ fn main() -> Result<(), tch::TchError> {
     let mut environment = rl_env::AzulEnv::new(0, config.max_timesteps_per_episode);
     let mut writer = SummaryWriter::new(format!(
         "runs/azul_ppo/{}-{}",
-        "asymmetric_upper_clip_AdamW",
+        "asymmetric_upper_clip",
         SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards")
