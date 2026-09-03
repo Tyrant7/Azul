0bf5ccc65c76f1b7704a89bd07172f9085baf5c0
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-09-02 23:50:51 -0700

    Reward scaling and gamma update

--- rl_env/src/main.rs
+++ rl_env/src/main.rs
@@ -5,12 +5,13 @@ fn main() -> Result<(), tch::TchError> {
         timesteps_per_batch: 1_000,
         max_timesteps_per_episode: 1_000,
         updates_per_iteration: 5,
+        gamma: 0.995,
         ..Default::default()
     };
 
     let mut trainer = rl_env::PpoTrainer::new(config)?;
     let mut environment = rl_env::AzulEnv::new(0, config.max_timesteps_per_episode);
-    let mut logger = logging::TrainingLogger::new("camf_split_LR");
+    let mut logger = logging::TrainingLogger::new("camf_split_LR_stability");
 
     logger.log_device();
     trainer.train_with_callback(&mut environment, 1_000_000, |metrics| logger.log(metrics));
