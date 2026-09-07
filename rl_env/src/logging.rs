f0f7a680033abd5df1957d81a226758ab2fcf044
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-09-07 12:27:19 -0700

    Fixed logging

--- rl_env/src/logging.rs
+++ rl_env/src/logging.rs
@@ -92,27 +92,29 @@ impl TrainingLogger {
             .add_scalar("episode/mean_winner_score", metrics.mean_winner_score, step);
         self.writer
             .add_scalar("episode/learner_win_rate", metrics.learner_win_rate, step);
-        self.writer
-            .add_scalar("evaluation/greedy_win_rate", metrics.greedy_win_rate, step);
-        self.writer
-            .add_scalar("evaluation/games", metrics.greedy_eval_games as f32, step);
-        self.writer.add_scalar(
-            "evaluation/opponents",
-            metrics.greedy_eval_opponents as f32,
-            step,
-        );
-        self.writer.add_scalar(
-            "evaluation/strongest_opponent_rating",
-            metrics.greedy_eval_strongest_opponent_rating,
-            step,
-        );
-        self.writer
-            .add_scalar("evaluation/current_elo", metrics.greedy_current_elo, step);
-        self.writer.add_scalar(
-            "evaluation/top_historical_elo",
-            metrics.greedy_top_historical_elo,
-            step,
-        );
+        if metrics.greedy_eval_games > 0 {
+            self.writer
+                .add_scalar("evaluation/greedy_win_rate", metrics.greedy_win_rate, step);
+            self.writer
+                .add_scalar("evaluation/games", metrics.greedy_eval_games as f32, step);
+            self.writer.add_scalar(
+                "evaluation/opponents",
+                metrics.greedy_eval_opponents as f32,
+                step,
+            );
+            self.writer.add_scalar(
+                "evaluation/strongest_opponent_rating",
+                metrics.greedy_eval_strongest_opponent_rating,
+                step,
+            );
+            self.writer
+                .add_scalar("evaluation/current_elo", metrics.greedy_current_elo, step);
+            self.writer.add_scalar(
+                "evaluation/top_historical_elo",
+                metrics.greedy_top_historical_elo,
+                step,
+            );
+        }
 
         for (player, values) in [("player_zero", 0), ("player_one", 1)] {
             let penalties_tag = format!("game/{player}_average_penalties");
@@ -148,8 +150,19 @@ impl TrainingLogger {
         }
         self.writer.flush();
 
+        let evaluation_summary = if metrics.greedy_eval_games > 0 {
+            format!(
+                " greedy_win_rate={:.3} current_elo={:.1} top_elo={:.1} eval_games={}",
+                metrics.greedy_win_rate,
+                metrics.greedy_current_elo,
+                metrics.greedy_top_historical_elo,
+                metrics.greedy_eval_games,
+            )
+        } else {
+            String::new()
+        };
         println!(
-            "iteration={} timesteps={} actor_loss={:.4} critic_loss={:.4} entropy={:.3} normalized_entropy={:.3} actor_grad={:.3} critic_grad={:.3} actor_update={:.5} critic_update={:.5} return={:.2}+-{:.2} value={:.2}+-{:.2} advantage={:.2}+-{:.2} explained_variance={:.3} score_diff={:.2} win_rate={:.3} greedy_win_rate={:.3} current_elo={:.1} top_elo={:.1} eval_games={}",
+            "iteration={} timesteps={} actor_loss={:.4} critic_loss={:.4} entropy={:.3} normalized_entropy={:.3} actor_grad={:.3} critic_grad={:.3} actor_update={:.5} critic_update={:.5} return={:.2}+-{:.2} value={:.2}+-{:.2} advantage={:.2}+-{:.2} explained_variance={:.3} score_diff={:.2} win_rate={:.3}{}",
             metrics.iteration,
             metrics.timesteps,
             metrics.actor_loss,
@@ -169,10 +182,7 @@ impl TrainingLogger {
             metrics.explained_variance,
             metrics.mean_final_score_difference,
             metrics.learner_win_rate,
-            metrics.greedy_win_rate,
-            metrics.greedy_current_elo,
-            metrics.greedy_top_historical_elo,
-            metrics.greedy_eval_games,
+            evaluation_summary,
         );
     }
 }
